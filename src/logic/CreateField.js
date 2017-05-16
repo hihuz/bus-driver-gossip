2efc5ebd41c7f766bd8921004e5cdef1796f9a12
Sylvain Boulade <bopulie@gmail.com>
2017-05-16 09:02:56 +0200

    Rename Playground to Field, add a few tests

--- src/logic/CreateField.js
+++ src/logic/CreateField.js
@@ -5,21 +5,27 @@ export const gatherGossips = (drivers = []) => {
   const length = drivers.reduce((acc, cur) => Math.max(acc, cur.route.length), 0);
   const gossips = new Array(length)
     .fill(undefined)
-    .map((stop, i) => drivers.reduce((acc, cur) => (cur.stop === i ? [...acc, cur.id] : acc), []));
+    .map((stop, i) =>
+      drivers
+        .reduce((acc, cur) => (cur.stop === i ? [...acc, ...cur.gossips] : acc), [])
+        .filter((gossip, idx, arr) => arr.indexOf(gossip) === idx)
+    );
   return gossips;
 };
 
-const CreatePlayground = ({ drivers = [] }) => {
+const CreateField = ({ drivers = [] }) => {
   const gossips = gatherGossips(drivers);
   return {
     drivers,
     count: 0,
     remaining: 480,
     gossips,
-    exchange() {},
+    exchange() {
+      this.gossips = gatherGossips(this.drivers);
+    },
     tick() {},
     run() {}
   };
 };
 
-export default CreatePlayground;
+export default CreateField;
--- src/logic/__tests__/CreateField.test.js
+++ src/logic/__tests__/CreateField.test.js
@@ -1,7 +1,7 @@
 /** *
  * /!\ This assumes no "empty" position in the input, as in every position from 0 to max is used
  *
- * Playground:
+ * Field:
  *  Props:
  *    drivers (array of Drivers),
  *    count (number of iterations),
@@ -15,13 +15,13 @@
  *    run (tick until the end)
  *
  ***/
-import CreatePlayground from "../CreatePlayground";
+import CreateField from "../CreateField";
 import CreateDrivers from "../CreateDrivers";
 import CreateDriver from "../CreateDriver";
 
-describe("CreatePlayground", () => {
+describe("CreateField", () => {
   test("should return an object with drivers/count/remaining/gossips default props", () => {
-    const Field = CreatePlayground({});
+    const Field = CreateField({});
     expect(Field.drivers).not.toEqual(undefined);
     expect(Field.count).not.toEqual(undefined);
     expect(Field.remaining).not.toEqual(undefined);
@@ -30,26 +30,29 @@ describe("CreatePlayground", () => {
 
   test("should init drivers based off passed input (array of drivers)", () => {
     const drivers = [
-      CreateDriver({ id: 0, route: [0, 2, 4] }),
-      CreateDriver({ id: 1, route: [1, 3, 5] })
+      CreateDriver({ id: 0, route: [0, 1, 2] }),
+      CreateDriver({ id: 1, route: [2, 1, 0] })
     ];
-    const Field = CreatePlayground({ drivers });
+    const Field = CreateField({ drivers });
     const expected = drivers;
     const actual = Field.drivers;
     expect(actual).toEqual(expected);
   });
 
   test("should init gossips based off passed input 1", () => {
-    const input = [[0, 1, 2], [0, 2, 3, 4]];
+    const input = [[0, 1, 2], [0, 3, 1, 2]];
     const drivers = CreateDrivers(input);
-    const Field = CreatePlayground({ drivers });
+    const Field = CreateField({ drivers });
     expect(Field.gossips[0]).toEqual([0, 1]);
+    expect(Field.gossips[1]).toEqual([]);
+    expect(Field.gossips[2]).toEqual([]);
+    expect(Field.gossips[3]).toEqual([]);
   });
 
   test("should init gossips based off passed input 2", () => {
-    const input = [[2, 1], [1, 3], [0, 1, 2], [0, 2, 3, 4]];
+    const input = [[2, 1], [1, 3], [0, 1, 2], [0, 2, 1, 3]];
     const drivers = CreateDrivers(input);
-    const Field = CreatePlayground({ drivers });
+    const Field = CreateField({ drivers });
     expect(Field.gossips[0]).toEqual([2, 3]);
     expect(Field.gossips[1]).toEqual([1]);
     expect(Field.gossips[2]).toEqual([0]);
@@ -57,31 +60,36 @@ describe("CreatePlayground", () => {
   });
 
   test("should have a exchange method", () => {
-    const Field = CreatePlayground({});
+    const Field = CreateField({});
     const expected = "function";
     const actual = typeof Field.exchange;
     expect(actual).toEqual(expected);
   });
 
   test("should have a tick method", () => {
-    const Field = CreatePlayground({});
+    const Field = CreateField({});
     const expected = "function";
     const actual = typeof Field.tick;
     expect(actual).toEqual(expected);
   });
 
   test("should have a run method", () => {
-    const Field = CreatePlayground({});
+    const Field = CreateField({});
     const expected = "function";
     const actual = typeof Field.run;
     expect(actual).toEqual(expected);
   });
 });
 
-describe("CreatePlayground: exchange", () => {
-  test("should update the gossips by calling talk on each driver", () => {
-    const input = [[0, 1, 2], [0, 2, 3, 4]];
+describe("CreateField: exchange", () => {
+  test("should gather the gossips at each stop 1", () => {
+    const input = [[0, 1, 2], [0, 3, 1, 2]];
     const drivers = CreateDrivers(input);
-    const Field = CreatePlayground({ drivers });
+    const Field = CreateField({ drivers });
+    Field.gossips = [];
+    Field.exchange();
+    const expected = [[0, 1], [], [], []];
+    const actual = Field.gossips;
+    expect(actual).toEqual(expected);
   });
 });
